63e082e72ceece7f55709cc8df8f3454ed392f44
Domenic Melcher <domi.m@outlook.com>
2021-07-11 18:00:34 +0200

    add lib for config handling

---
+++ src/lib/config/file/load.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+
+const createFile = require('./create');
+
+const supportedFiletypes = ['.json'];
+
+module.exports = (path, create) => {
+  if (!fs.existsSync(path)) {
+    if (create) {
+      createFile(path, 'json');
+    } else {
+      throw new Error(`'${path}' does not exist`);
+    }
+  }
+  supportedFiletypes.forEach((item) => {
+    if (!path.endsWith(item))
+      throw new Error(`'${path}': is not a ${item} file`);
+  });
+  return require(path);
+};
